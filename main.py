e1937be2cee7d9fc7abe93c01ab933fefb33b32f
Yohannes Getu <nede.evoli@gmail.com>
2021-07-31 11:28:21 +0300

    added Candlestick charts

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pandas_datareader import data
 import datetime
+from bokeh.plotting import figure, show, output_file 
 
-start = datetime.datetime(2021,3,1)
-end = datetime.datetime(2021,3,10)
+start = datetime.datetime(2021,7,1)
+end = datetime.datetime(2021,7,10)
 
 df = data.DataReader(name="AAPL", data_source="yahoo", start=start, end=end)
+
+p = figure(x_axis_type='datetime', width=1000, height=300)
+p.title.text = "Candlestick Chart"
 
 print(df)
