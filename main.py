0dd33772d15d81b0f6bdafbdc4280eaad421e9a2
Yohannes Getu <nede.evoli@gmail.com>
2021-08-02 16:08:28 +0300

    updated chart style

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ df["Status"] = [inc_dec(c,o) for c, o in zip(df.Close, df.Open)]
 df["Middle"] = (df.Open + df.Close)/2
 df["Height"] = abs(df.Close - df.Open)
 
-p = figure(x_axis_type='datetime', width=1000, height=300)
+p = figure(x_axis_type='datetime', width=1000, height=300, sizing_mode="scale_width")
 p.title.text = "Candlestick Chart"
 p.grid.grid_line_alpha = 0.3
 
@@ -29,10 +29,10 @@ hours_12 = 12*60*60*1000
 p.segment(df.index, df.High, df.index, df.Low, color="Black")
 
 p.rect(df.index[df.Status=="Increase"], df.Middle[df.Status=="Increase"], hours_12,
-        df.Height[df.Status=="Increase"], fill_color="green", line_color="black")
+        df.Height[df.Status=="Increase"], fill_color="#CCFFFF", line_color="black")
 
 p.rect(df.index[df.Status=="Decrease"], df.Middle[df.Status=="Decrease"], hours_12,
-       df.Height[df.Status=="Decrease"], fill_color="red", line_color="black")
+       df.Height[df.Status=="Decrease"], fill_color="#FF3333", line_color="black")
 
 
 output_file("CS.html")
