5203ea5cffd4d078487e9c9e40be74bec25ec2ba
Yohannes Getu <nede.evoli@gmail.com>
2021-08-02 15:58:33 +0300

    added segment

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ df["Height"] = abs(df.Close - df.Open)
 
 p = figure(x_axis_type='datetime', width=1000, height=300)
 p.title.text = "Candlestick Chart"
+p.grid.grid_line_alpha = 0.3
 
 hours_12 = 12*60*60*1000
+
+p.segment(df.index, df.High, df.index, df.Low, color="Black")
 
 p.rect(df.index[df.Status=="Increase"], df.Middle[df.Status=="Increase"], hours_12,
         df.Height[df.Status=="Increase"], fill_color="green", line_color="black")
 
 p.rect(df.index[df.Status=="Decrease"], df.Middle[df.Status=="Decrease"], hours_12,
        df.Height[df.Status=="Decrease"], fill_color="red", line_color="black")
+
 
 output_file("CS.html")
 show(p)
